8ce8ff706ae54dbdb7c4098f90c1d22e57f6e72a
Ali-crypto-rgb <ali.edalat89@gmail.com>
2021-06-02 14:00:19 +0200

    added dev name to game

--- src/main/java/edu/agile/model/Game.java
+++ src/main/java/edu/agile/model/Game.java
@@ -13,6 +13,7 @@ public class Game {
     private String name;
     private String description;
     private File imageFile;
+    private String developerName;
 
     @Override
     public String toString() {
