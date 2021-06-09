75e38485cbe5b633b280805145f4f56c6f9a0b1e
Denkaan <dennis.petreski@hotmail.com>
2021-06-09 14:00:02 +0200

    #17 - library

--- src/main/java/edu/agile/Utils/Program.java
+++ src/main/java/edu/agile/Utils/Program.java
@@ -7,18 +7,20 @@ import java.io.IOException;
 
 public class Program {
     public static void run(Game game) {
-        String gamePath = game.getGame().getAbsolutePath();
-        String fileType = FilenameUtils.getExtension(gamePath);
 
-        try {
-            if ("exe".equals(fileType)) {
-                Runtime.getRuntime().exec(gamePath);
-            } else if ("jar".equals(fileType)) {
-                Runtime.getRuntime().exec("javaw -jar " + gamePath);
+        if (game != null) {
+            String gamePath = game.getGame().getAbsolutePath();
+            String fileType = FilenameUtils.getExtension(gamePath);
+
+            try {
+                if ("exe".equals(fileType)) {
+                    Runtime.getRuntime().exec(gamePath);
+                } else if ("jar".equals(fileType)) {
+                    Runtime.getRuntime().exec("javaw -jar " + gamePath);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
     }
 }
